fea9021d1492c530ac6f7ede9bbde83ecdb50c7a
playdeax <62872027+playdeax@users.noreply.github.com>
2021-04-12 13:24:26 +0700

    DropPlatform

---
+++ Assets/DropPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.EndsWith("Ball")) {
+            StartCoroutine(WaitAndDrop(0.3f));
+        }
+    }
+    void Update()
+    {
+        
+    }
+    private IEnumerator WaitAndDrop(float waitTime)
+    {
+       yield return new WaitForSeconds(waitTime);
+        transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        transform.GetComponent<Rigidbody2D>().gravityScale = 2;
+       transform.GetComponent<Collider2D>().enabled = false;
+    }
+}
