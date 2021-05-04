0645cf1efb7aa860d3854b5600dc66c4db472163
tavantruong222@gmail.com <tavantruong222@gmail.com>
2021-05-04 17:40:56 +0700

    start build boss 1

---
+++ Assets/BlueBall/Scripts/Boss/Boss1.cs
@@ -0,0 +1,19 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace BlueBall.Scripts.Boss
+{
+    public class Boss1 : EnemyBase
+    {
+        [SerializeField] private EnemyBaseBeHit beHit;
+        [SerializeField] private float jumpDelay = 1.5f;
+        [SerializeField,MinMaxSlider(1, 10, true)] private Vector2 jumpDistance;
+        
+        
+        public override void CollisionWithBall()
+        {
+            base.CollisionWithBall();
+            GameLevelManager.Ins.SetBallBeHit(speedAddBall);
+        }
+    }
+}
