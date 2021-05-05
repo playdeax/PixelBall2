8865817040987753fcc1323e8d6670e8ceba3389
tavantruong222@gmail.com <tavantruong222@gmail.com>
2021-05-05 09:10:27 +0700

    no message

--- Assets/BlueBall/Scripts/Boss/Boss1.cs
+++ Assets/BlueBall/Scripts/Boss/Boss1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -6,20 +6,49 @@ namespace BlueBall.Scripts.Boss
 {
     public class Boss1 : EnemyBase
     {
-        [SerializeField] private EnemyBaseBeHit beHit;
+        [SerializeField] private int healthPoint;
         [SerializeField] private float jumpDelay = 1.5f;
         [SerializeField,MinMaxSlider(1, 10, true)] private Vector2 jumpDistance;
-
-
+        [SerializeField] private EnemyBaseBeHit beHit;
+        [SerializeField] private SpriteRenderer spriteEnemy;
+        [SerializeField] private ParticleSystem efxBeHit;
+        
         private void Start()
         {
-            
+            beHit.gameObject.SetActive(false);
         }
 
         public override void CollisionWithBall()
         {
             base.CollisionWithBall();
             GameLevelManager.Ins.SetBallBeHit(speedAddBall);
+        }
+        
+        public override void SetEnemyBeHit(Vector2 _forceAddBall2)
+        {
+            base.SetEnemyBeHit(_forceAddBall2);
+            healthPoint--;
+            if(healthPoint>0)
+                StartCoroutine(SetEnemyBeHit_IEnumerator());
+            else
+                GameLevelManager.Ins.SetBall_KillEnemy(_forceAddBall2);
+        }
+
+        public IEnumerator SetEnemyBeHit_IEnumerator()
+        {
+            SetCollier_Enable(false);
+            //spriteEnemy.DOFade(0f, 0.2f).SetEase(Ease.OutQuart);
+
+            efxBeHit.gameObject.SetActive(true);
+            efxBeHit.Play();
+            yield return new WaitForSeconds(2f);
+            Destroy(gameObject);
+        }
+
+        private void SetBossStun()
+        {
+            SetCollier_Enable(false);
+            
         }
     }
 }
---
+++ Assets/BlueBall/Scripts/Boss/BossHPGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlueBall.Scripts.Boss
+{
+    public class BossHPGroup : MonoBehaviour
+    {
+        [SerializeField] private Image hpBar;
+
+        private int _maxHP;
+        private int _currentHP;
+        public void Init(int maxHP)
+        {
+            _maxHP = maxHP;
+            _currentHP = _maxHP;
+            hpBar.fillAmount = 1f;
+        }
+
+        public void DecreaseHP(int decrease = 1)
+        {
+            _currentHP -= decrease;
+            hpBar.fillAmount = _currentHP * 1f / _maxHP;
+        }
+        
+    }
+}
