391d2f87c279e6d19c38f4e3c40aef8570d11fa3
tavantruong222@gmail.com <tavantruong222@gmail.com>
2021-05-07 07:49:58 +0700

    add preview in win popup

--- Assets/BlueBall/Scripts/WinPopup.cs
+++ Assets/BlueBall/Scripts/WinPopup.cs
@@ -70,7 +70,7 @@ public class WinPopup : MonoBehaviour
         btnBackBallPreview.OnPointerClickCallBack_Completed.AddListener(TouchBackBallPreview);
         btnTryBallPreview.OnPointerClickCallBack_Completed.AddListener(TouchTryBallPreview);
         btnActiveBallPreview.OnPointerClickCallBack_Completed.AddListener(TouchActiveBallPreview);
-
+        InitBallPreview();
         ShowBallPreview();
     }
 
@@ -116,7 +116,7 @@ public class WinPopup : MonoBehaviour
         FirebaseManager.instance.LogLevelWin(_level);
 
         StartCoroutine(ShowPopup_IEnumerator(coinReward));
-        InitBallPreview();
+        //InitBallPreview();
     }
 
     public IEnumerator ShowPopup_IEnumerator(int coinReward)
@@ -194,7 +194,29 @@ public class WinPopup : MonoBehaviour
         
         btnBackBallPreview.gameObject.SetActive(true);
         btnBackBallPreview.GetComponent<BBUIView>().ShowView();
-
+        if (Config.GetBallActive() == listIDBallPreviews[indexBallPreview])
+        {
+            btnTryBallPreview.gameObject.SetActive(false);
+            btnActiveBallPreview.gameObject.SetActive(false);
+        }
+        else if (Config.GetInfoBallFromID(listIDBallPreviews[indexBallPreview]).ballType == Config.BALL_TYPE.PREMIUM && Config.GetBuyIAP(Config.IAP_ID.premium_pack))
+        {
+            btnTryBallPreview.gameObject.SetActive(false);
+            btnActiveBallPreview.gameObject.SetActive(true);
+            btnActiveBallPreview.GetComponent<BBUIView>().ShowView();
+        }
+        else if (Config.GetInfoBallUnlock(listIDBallPreviews[indexBallPreview]))
+        {
+            btnTryBallPreview.gameObject.SetActive(false);
+            btnActiveBallPreview.gameObject.SetActive(true);
+            btnActiveBallPreview.GetComponent<BBUIView>().ShowView();
+        }
+        else
+        {
+            btnActiveBallPreview.gameObject.SetActive(false);
+            btnTryBallPreview.gameObject.SetActive(true);
+            btnTryBallPreview.GetComponent<BBUIView>().ShowView();
+        }
         yield return new WaitForSeconds(0.5f);
         SoundManager.instance.SFX_PhaoHoa();
         efxWin.gameObject.SetActive(true);
@@ -369,12 +391,14 @@ public class WinPopup : MonoBehaviour
     
     #region BALL PREVIEW
     public void ShowBallPreview() {
+        
         if (Config.currInfoBall_Try != null)
         {
             ballAnimator.runtimeAnimatorController = Config.currInfoBall_Try.animatorImgOverrideController;
         }
         else {
-            ballAnimator.runtimeAnimatorController = Config.GetInfoBallFromID(Config.GetBallActive()).animatorImgOverrideController;
+//            ballAnimator.runtimeAnimatorController = Config.GetInfoBallFromID(Config.GetBallActive()).animatorImgOverrideController;
+            ballAnimator.runtimeAnimatorController = Config.GetInfoBallFromID(listIDBallPreviews[indexBallPreview]).animatorImgOverrideController;
         }
 
 
@@ -385,13 +409,27 @@ public class WinPopup : MonoBehaviour
 
     private void InitBallPreview()
     {
+        var lockedIds = new List<int>();
         for (int i = 0; i < listIDBallPreviews.Count; i++)
         {
-            if (listIDBallPreviews[i] == Config.GetBallActive())
+            if (!Config.GetInfoBallUnlock(listIDBallPreviews[i]))
             {
-                indexBallPreview = i;
-                return;
+                lockedIds.Add(listIDBallPreviews[i]);
             }
+        }
+        
+        Debug.Log("lockedIds.Count = "+lockedIds.Count);
+        var percent = Random.Range(0, 100f);
+        if (lockedIds.Count > 0)
+        {
+            var randomId = Random.Range(0, lockedIds.Count);
+            var index = listIDBallPreviews.IndexOf(lockedIds[randomId]);
+            indexBallPreview = index;
+        }
+        else
+        {
+            var index = listIDBallPreviews.IndexOf(Config.GetBallActive());
+            indexBallPreview = index;
         }
     }
     private void TouchNextBallPreview()
